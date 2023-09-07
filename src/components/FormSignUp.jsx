20f2365ba2bbd65362dca885dedd7c31ca2b7c7e
maira hernandez <mairahdez@gmail.com>
2023-09-07 08:50:15 -0400

    clase 2 Utiliza MateriaUi

--- src/components/FormSignUp.jsx
+++ src/components/FormSignUp.jsx
@@ -1,8 +1,21 @@
+import { Button } from "@mui/material"
+
 function FormSignUp() {
     return (
     <form>
+        <label >Nombre</label>
        <input type="text" />
-       <button>Registrarse</button>
+       <label >Apellidos</label>
+       <input type="text" />
+       <label >Correo electronico</label>
+       <input type="email" />
+       <label >Promociones</label>
+       <input type="checkbox" />
+       <label >Novedades</label>
+       <input type="checbox" />
+       <Button variant="contained" color="success">
+        Registrarse
+        </Button>
         </form>
     )
 }
