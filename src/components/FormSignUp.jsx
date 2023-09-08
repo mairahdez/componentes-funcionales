742351c7c2413e620ce44ccad4d970ec1c31e29a
maira hernandez <mairahdez@gmail.com>
2023-09-07 21:22:35 -0400

    Modificado

--- src/components/FormSignUp.jsx
+++ src/components/FormSignUp.jsx
@@ -1,19 +1,48 @@
 import { Button } from "@mui/material"
+import { TextField } from '@mui/material';
+import { Switch } from "@mui/material"
+import FormGroup from '@mui/material/FormGroup';
+import FormControlLabel from '@mui/material/FormControlLabel';
+
 
 function FormSignUp() {
     return (
     <form>
-        <label >Nombre</label>
-       <input type="text" />
-       <label >Apellidos</label>
-       <input type="text" />
-       <label >Correo electronico</label>
-       <input type="email" />
-       <label >Promociones</label>
-       <input type="checkbox" />
-       <label >Novedades</label>
-       <input type="checbox" />
-       <Button variant="contained" color="success">
+        <TextField 
+            id="name"
+            label="Nombre"
+            variant="outlined"
+            fullWidth
+            margin="normal"
+        />
+        <TextField 
+            id="lastName"
+            label="Apellidos"
+            variant="outlined"
+            fullWidth
+            margin="normal"
+        />
+        <TextField 
+            id="email"
+            label="Email"
+            variant="outlined"
+            fullWidth
+            margin="normal"
+        />
+       <FormGroup>
+            <FormControlLabel control={
+                <Switch defaultChecked
+                />
+            } label="Deseo recibir Promociones" />
+       
+            <FormControlLabel
+              control={
+              <Switch defaultChecked />
+            } 
+                label="Deseo recibir Novedades"
+            />
+       </FormGroup>
+       <Button variant="contained">
         Registrarse
         </Button>
         </form>
