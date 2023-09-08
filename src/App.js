739380ce4d9bbe013d899a28ac481ed19b3630c6
maira hernandez <mairahdez@gmail.com>
2023-09-07 21:19:11 -0400

    modificado

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
+import { Container } from '@mui/material';
 import FormSignUp from './components/FormSignUp';
+import { Typography } from '@mui/material';
 
 function App() {
   return (
-    <>
-    <h1>Formulario de Registro</h1>
+    <Container component="section" maxWidth="sm" >
+    <Typography variant='h3' align='center' component="h1">Formulario de Registro</Typography>
      <FormSignUp />
-    </>
+    </Container>
   );
 }
 
